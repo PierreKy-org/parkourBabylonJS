38fac320b6cf7216af537d1bd7890b951ba52339
QuentinBeauchet <quentinbeauchet@free.fr>
2022-03-24 19:29:47 +0100

    rond point

--- js/elements/Rotator.js
+++ js/elements/Rotator.js
@@ -17,7 +17,7 @@ class Rotator extends BigBoundingBox {
 
 class RotatorFR extends Rotator {
   onPlayerCollision() {
-    console.log(" RotatorFR");
+    console.log("RotatorFR");
     this.scene.player.orientation = "right";
     BABYLON.Animation.CreateAndStartAnimation(
       "rotateFR",
@@ -40,7 +40,8 @@ class RotatorFR extends Rotator {
 
 class RotatorFL extends Rotator {
   onPlayerCollision() {
-    console.log(" RotatorFL");
+    console.log("RotatorFL");
+    console.log(BABYLON.Tools.ToDegrees(this.scene.camera.alpha));
     this.scene.player.orientation = "left";
     BABYLON.Animation.CreateAndStartAnimation(
       "rotateFL",
@@ -49,7 +50,7 @@ class RotatorFL extends Rotator {
       1.5,
       1,
       BABYLON.Tools.ToRadians(270),
-      BABYLON.Tools.ToRadians(180),
+      BABYLON.Tools.ToRadians(360),
       BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
     );
 
@@ -66,14 +67,34 @@ class RotatorBR extends Rotator {
 }
 
 class RotatorBL extends Rotator {
-  onPlayerCollision() {}
+  onPlayerCollision() {
+    console.log("RotatorBL");
+    this.scene.player.orientation = "right";
+    BABYLON.Animation.CreateAndStartAnimation(
+      "rotateBL",
+      this.scene.camera,
+      "alpha",
+      1.5,
+      1,
+      BABYLON.Tools.ToRadians(90),
+      BABYLON.Tools.ToRadians(180),
+      BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
+    );
+
+    this.scene.player.resetRotation();
+    let av = this.scene.player.mesh.physicsImpostor.getAngularVelocity();
+    this.scene.player.mesh.physicsImpostor.setAngularVelocity(new BABYLON.Vector3(av.z, 0, av.x));
+    let al = this.scene.player.mesh.physicsImpostor.getLinearVelocity();
+    this.scene.player.mesh.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(-al.z, al.y, al.x));
+  }
 }
 
 class RotatorRF extends Rotator {
   onPlayerCollision() {
+    console.log("RotatorRF");
     this.scene.player.orientation = "front";
     BABYLON.Animation.CreateAndStartAnimation(
-      "rotateFL",
+      "rotateRF",
       this.scene.camera,
       "alpha",
       1.5,
@@ -100,5 +121,24 @@ class RotatorLF extends Rotator {
 }
 
 class RotatorLB extends Rotator {
-  onPlayerCollision() {}
+  onPlayerCollision() {
+    console.log("RotatorLB");
+    this.scene.player.orientation = "back";
+    BABYLON.Animation.CreateAndStartAnimation(
+      "rotateLB",
+      this.scene.camera,
+      "alpha",
+      1.5,
+      1,
+      BABYLON.Tools.ToRadians(0),
+      BABYLON.Tools.ToRadians(90),
+      BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
+    );
+
+    this.scene.player.resetRotation();
+    let av = this.scene.player.mesh.physicsImpostor.getAngularVelocity();
+    this.scene.player.mesh.physicsImpostor.setAngularVelocity(new BABYLON.Vector3(av.z, 0, av.x));
+    let al = this.scene.player.mesh.physicsImpostor.getLinearVelocity();
+    this.scene.player.mesh.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(-al.z, al.y, al.x));
+  }
 }
