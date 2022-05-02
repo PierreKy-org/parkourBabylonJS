e8eedfa3db6aa63cff7a778f707a6983dad6cd05
QuentinBeauchet <quentinbeauchet@free.fr>
2022-05-02 23:08:42 +0200

    rotator

--- public/js/elements/Rotator.js
+++ public/js/elements/Rotator.js
@@ -13,34 +13,32 @@ class Rotator {
     }
 
     if (!Rotator.builder) {
-      Rotator.builder = BABYLON.MeshBuilder.CreateBox("box", {
-        height: 1,
-        width: 1,
-        depth: 1,
-      });
+      Rotator.builder = BABYLON.MeshBuilder.CreateTorus("torus", { tessellation: 6, thickness: 0.5, diameter: 2 });
+      Rotator.builder.rotation.z = Math.PI / 2;
+
       Rotator.builder.name = `rotator_${pX}_${pY}_${pZ}`;
 
-      Rotator.builder.material = new BABYLON.StandardMaterial("simpleMaterial");
-      Rotator.builder.material.disableLighting = true;
-      Rotator.builder.material.emissiveColor = BABYLON.Color3.White();
-      Rotator.builder.material.diffuseTexture = new BABYLON.Texture("../assets/arrow.png", this.scene.scene);
+      Rotator.builder.material = this.scene.assetsManager.Materials["Simple #NJXV5A#12"];
+
+      this.plane = BABYLON.Mesh.CreatePlane("rotator_arrow", 1.5);
+      this.plane.parent = Rotator.builder;
+      this.plane.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
+      this.plane.position.y = 1.5;
+
+      var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(this.plane);
+
+      advancedTexture.parseContent(this.scene.assetsManager.Textures["Arrow"]);
 
       this.box = Rotator.builder;
     } else {
       this.box = Rotator.builder.createInstance(`rotator_${pX}_${pY}_${pZ}`);
     }
     this.box.alwaysSelectAsActiveMesh = true;
-    this.box.position = new BABYLON.Vector3(pX, pY, pZ);
+    this.box.position = new BABYLON.Vector3(pX, pY + 0.1, pZ);
   }
 
   setPhysics() {
     this.box.checkCollisions = true;
-    this.box.physicsImpostor = new BABYLON.PhysicsImpostor(
-      this.box,
-      BABYLON.PhysicsImpostor.BoxImpostor,
-      { mass: 0 },
-      this.scene.scene
-    );
 
     this.box.actionManager = new BABYLON.ActionManager(this.scene.scene);
     this.box.actionManager.registerAction(
@@ -54,14 +52,13 @@ class Rotator {
         () => this.onPlayerCollision()
       )
     );
-
-    this.box.showBoundingBox = true;
+    /*
     this.box.setBoundingInfo(
       new BABYLON.BoundingInfo(
         BABYLON.Vector3.Minimize(this.box.getBoundingInfo().boundingBox.minimum, new BABYLON.Vector3(0, 0, 0)),
         BABYLON.Vector3.Maximize(this.box.getBoundingInfo().boundingBox.maximum, new BABYLON.Vector3(0, 10, 0))
       )
-    );
+    );*/
   }
 
   rotate(orientation, from, to, angular, linear) {
@@ -90,48 +87,80 @@ class Rotator {
 }
 
 class RotatorFR extends Rotator {
+  constructor(pX, pY, pZ, scene) {
+    super(pX, pY, pZ, scene);
+  }
+
   onPlayerCollision() {
     this.rotate("right", 270, 180, new BABYLON.Vector3(1, 0, 1), new BABYLON.Vector3(-1, 1, -1));
   }
 }
 
 class RotatorFL extends Rotator {
+  constructor(pX, pY, pZ, scene) {
+    super(pX, pY, pZ, scene);
+  }
+
   onPlayerCollision() {
     this.rotate("left", 270, 360, new BABYLON.Vector3(-1, 0, 1), new BABYLON.Vector3(1, 1, 1));
   }
 }
 
 class RotatorBR extends Rotator {
+  constructor(pX, pY, pZ, scene) {
+    super(pX, pY, pZ, scene);
+  }
+
   onPlayerCollision() {
     this.rotate("right", 90, 180, new BABYLON.Vector3(-1, 0, 1), new BABYLON.Vector3(1, 1, 1));
   }
 }
 
 class RotatorBL extends Rotator {
+  constructor(pX, pY, pZ, scene) {
+    super(pX, pY, pZ, scene);
+  }
+
   onPlayerCollision() {
     this.rotate("left", 90, 180, new BABYLON.Vector3(1, 0, 1), new BABYLON.Vector3(1, 1, 1));
   }
 }
 
 class RotatorRF extends Rotator {
+  constructor(pX, pY, pZ, scene) {
+    super(pX, pY, pZ, scene);
+  }
+
   onPlayerCollision() {
     this.rotate("front", 180, 270, new BABYLON.Vector3(1, 0, 1), new BABYLON.Vector3(-1, 1, 1));
   }
 }
 
 class RotatorRB extends Rotator {
+  constructor(pX, pY, pZ, scene) {
+    super(pX, pY, pZ, scene);
+  }
+
   onPlayerCollision() {
     this.rotate("back", 180, 90, new BABYLON.Vector3(1, 0, -1), new BABYLON.Vector3(1, 1, 1));
   }
 }
 
 class RotatorLF extends Rotator {
+  constructor(pX, pY, pZ, scene) {
+    super(pX, pY, pZ, scene);
+  }
+
   onPlayerCollision() {
     this.rotate("front", 360, 270, new BABYLON.Vector3(1, 0, -1), new BABYLON.Vector3(1, 1, 1));
   }
 }
 
 class RotatorLB extends Rotator {
+  constructor(pX, pY, pZ, scene) {
+    super(pX, pY, pZ, scene);
+  }
+
   onPlayerCollision() {
     this.rotate("back", 0, 90, new BABYLON.Vector3(1, 0, 1), new BABYLON.Vector3(-1, 1, 1));
   }
--- public/js/main.js
+++ public/js/main.js
@@ -46,6 +46,7 @@ window.onload = () => {
           textures: [
             "../../assets/textures/end.json",
             "../../assets/textures/esc.json",
+            "../../assets/textures/arrow.json",
           ],
           audio: {
             jump: { path: "../../assets/audio/jump.wav", loop: false },
