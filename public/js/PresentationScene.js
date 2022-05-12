9a3f94793567e48e593a27b71ff0a275385c6556
Zorrino <pierrekouyoumdjian@hotmail.fr>
2022-05-12 11:55:27 +0200

    Menu button is moving

--- public/js/PresentationScene.js
+++ public/js/PresentationScene.js
@@ -13,6 +13,44 @@ export default class PresentationScene {
       await this.advancedTexture.parseFromURLAsync("../assets/materials/guiPresentation.json");
       
   
+      var animationHideText = new BABYLON.Animation("myAnimation", "width", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+      var keys = []; 
+  
+      keys.push({
+          frame: 0,
+          value: 0.12
+      });
+  
+      keys.push({
+          frame: 30,
+          value: 0.1
+      });
+      keys.push({
+        frame: 60,
+        value: 0.12
+    });
+
+
+    var animationshowText = new BABYLON.Animation("myAnimation", "height", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+    var keyss = []; 
+
+    keyss.push({
+        frame: 0,
+        value: 0.04
+    });
+
+    keyss.push({
+        frame: 30,
+        value: 0.03
+    });
+    keyss.push({
+      frame: 60,
+      value: 0.04
+  });
+      animationshowText.setKeys(keyss);
+      animationHideText.setKeys(keys);
+      this.advancedTexture.getControlByName("bouton").animations = [];
+      this.scene.beginDirectAnimation(this.advancedTexture.getControlByName("bouton"), [animationHideText, animationshowText], 0, 100, true);
       //Mode facile
       this.advancedTexture.getControlByName("bouton").onPointerUpObservable.add(() => {
         window.changeScene(1);
